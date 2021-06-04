b78830bc8c591354f2e2fe5d7ab14b22add62885
Hubert Leszkiewicz <zerotwoswift@gmail.com>
2021-06-04 23:31:50 +0100

    Second Commit

---
+++ zts portfolio/Items-CoreDataHelpers.swift
@@ -0,0 +1,35 @@
+//
+//  Items-CoreDataHelpers.swift
+//  zts portfolio
+//
+//  Created by Hubert Leszkiewicz on 04/06/2021.
+//
+
+import Foundation
+
+extension Item {
+    var itemTitle: String {
+        title ?? ""
+    }
+    
+    var itemDetail: String {
+        detail ?? ""
+    }
+    
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+    
+    static var example: Item {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+        
+        return item
+    }
+}
---
+++ zts portfolio/Project-CoreDataHelpers.swift
@@ -0,0 +1,68 @@
+//
+//  PRoject-CoreDataHelpers.swift
+//  zts portfolio
+//
+//  Created by Hubert Leszkiewicz on 04/06/2021.
+//
+
+import Foundation
+
+extension Project {
+    var projectTitle: String {
+        title ?? "New Project"
+    }
+    
+    var projectDetail: String {
+        detail ?? ""
+    }
+    
+    var projectColor: String {
+        color ?? "Light Blue"
+    }
+    
+    var projectItems: [Item] {
+        let itemsArray = items?.allObjects as? [Item] ?? []
+        
+        return itemsArray.sorted { first, second in
+            if first.completion == false {
+                if second.completion == true {
+                    return true
+                }
+            } else if first.completion == true {
+               if second.completion == false {
+                    return false
+                }
+            }
+            
+            if first.priority > second.priority {
+                return true
+            } else if first.priority < second.priority {
+                return false
+            }
+            
+            return first.itemCreationDate < second.itemCreationDate
+        }
+    }
+    
+    var completionAmount: Double {
+        let originalItems = items?.allObjects as? [Item] ?? []
+        guard originalItems.isEmpty == false else { return 0 }
+        
+        let completedItems = originalItems.filter(\.completion)
+        return Double(completedItems.count) / Double(completedItems.count)
+    }
+    
+    static var example: Project {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let project = Project(context: viewContext)
+        project.title = "Example Project"
+        project.detail = "This is an example project"
+        project.closed = true
+        project.creationDate = Date()
+        
+        return project
+    }
+    
+}
