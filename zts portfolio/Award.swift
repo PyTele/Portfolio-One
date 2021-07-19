e78852fa993e4789ebec06ae95f82490cfd1449c
Hubert Leszkiewicz <zerotwoswift@gmail.com>
2021-07-19 22:40:01 +0100

    9.0

---
+++ zts portfolio/Award.swift
@@ -0,0 +1,22 @@
+//
+//  Award.swift
+//  zts portfolio
+//
+//  Created by Hubert Leszkiewicz on 19/07/2021.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    var id: String { name }
+    
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+    
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
