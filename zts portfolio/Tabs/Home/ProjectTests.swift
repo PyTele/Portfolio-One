16e7af0bfd8318362a208a7a6917635de03a3a48
Hubert Leszkiewicz <zerotwoswift@gmail.com>
2021-09-22 19:57:40 +0100

    1.0.19
    Added award and project tests

---
+++ zts portfolio/Tabs/Home/ProjectTests.swift
@@ -0,0 +1,40 @@
+//
+//  ProjectTests.swift
+//  zts portfolioTests
+//
+//  Created by Hubert Leszkiewicz on 22/09/2021.
+//
+
+import CoreData
+import XCTest
+@testable import zts_portfolio
+
+class ProjectTests: BaseTestCase {
+    func creatingProjectsAndItems() {
+        let targetCount = 10
+
+        for _ in 0..<targetCount {
+            let project = Project(context: managedObjectContext)
+
+            for _ in 0..<targetCount {
+                let item  = Item(context: managedObjectContext)
+                item.project = project
+            }
+        }
+
+        XCTAssertEqual(dataController.count(for: Project.fetchRequest()), targetCount)
+        XCTAssertEqual(dataController.count(for: Item.fetchRequest()), targetCount * targetCount)
+    }
+
+    func testDeletingProjectCascadeDeletesItems() throws {
+        try dataController.createSampleData()
+
+        let request = NSFetchRequest<Project>(entityName: "Project")
+        let projects = try managedObjectContext.fetch(request)
+
+        dataController.delete(projects[0])
+
+        XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 4)
+        XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 40)
+    }
+}
